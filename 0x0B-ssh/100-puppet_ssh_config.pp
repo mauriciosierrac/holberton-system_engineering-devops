2f5f8dba7011ccc5da0e11435174c9345d8caf45
mauricio.sierrac <2376@holbertonschool.com>
2021-04-03 22:13:36 +0000

    create task100 advanced

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,19 @@
-#!/usr/bin/env bash
+# this puppet file
+
+file { 'ssh_config':
+ensure  =>  'present',
+} ->
+
+file_line { 'Turn off passwd auth':
+  path    =>  '/etc/ssh/ssh_config',
+  replace =>  true,
+  match   =>  '#   PasswordAuthentication no',
+  line    =>  'PasswordAuthentication no',
+}
+
+file_line {  'Declare identity file':
+  path    =>  '/etc/ssh/ssh_config',
+  replace =>  true,
+  match   =>  '#   IdentityFile ~/.ssh/holberton',
+  line    =>  'IdentityFile ~/.ssh/holberton',
+}
