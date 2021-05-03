ba81dd8accab7c9f35c2fbfa17c4cb6e1fd8f0d6
mauricio.sierrac <2376@holbertonschool.com>
2021-05-03 22:09:59 +0000

    task01

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+import csv
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    user = requests.get(url + 'users/{}'.format(int(argv[1]))).json()
+    todos = requests.get(url + 'todos?userId={}'.format(int(argv[1]))).json()
+
+    with open('{}.csv'.format(argv[1]), 'w') as file:
+        res = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
+        for task in todos:
+            res.writerow([task.get('userId'), user.get('username'),
+                          task.get('completed'), task.get('title')])
