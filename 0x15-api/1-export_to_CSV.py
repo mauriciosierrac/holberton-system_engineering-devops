f105bf675663c79ee726c1993b2e99cf6041bb9b
mauricio.sierrac <2376@holbertonschool.com>
2021-05-03 22:38:36 +0000

    task01

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+''' this application export to csv file'''
+
 import requests
 from sys import argv
 import csv
@@ -9,7 +11,7 @@ if __name__ == '__main__':
     user = requests.get(url + 'users/{}'.format(int(argv[1]))).json()
     todos = requests.get(url + 'todos?userId={}'.format(int(argv[1]))).json()
 
-    with open('USER_ID.csv', 'w') as file:
+    with open('{}.csv'.format(argv[1]), 'w') as file:
         res = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
         for task in todos:
             res.writerow([task.get('userId'), user.get('username'),
