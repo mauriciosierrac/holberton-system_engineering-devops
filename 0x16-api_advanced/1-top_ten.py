315d989ca8209d0864fc5bfe5aa14394afad87e8
mauriciosierrac <2376@holbertonschool.com>
2021-05-17 15:23:23 -0500

    file: update header in get petition, task01

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -14,7 +14,7 @@ def top_ten(subreddit):
     """
 
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    
+
     headers = {
         "User-Agent": "linux:task00.api:v1.0.0 (by /u/Holberton2020)"
     }
@@ -23,7 +23,7 @@ def top_ten(subreddit):
         "limit": 10
     }
 
-    response = requests.get(url, headers=headers ,params=params)
+    response = requests.get(url, headers=headers, params=params)
     if (response is None):
         print('None')
         return
