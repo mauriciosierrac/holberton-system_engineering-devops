e0fc4f2f3f70e4783f83b82031f3342aff599518
mauriciosierrac <2376@holbertonschool.com>
2021-05-17 15:03:18 -0500

    file: updaate header in task00

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,14 +9,14 @@ def number_of_subscribers(subreddit):
         subreddit: param to argv[1]
     """
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    
+
     headers = {
         "User-Agent": "linux:task00.api:v1.0.0 (by /u/Holberton2020)"
     }
-    
-    response = requests.get(url, headers=headers)
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
     print(response.status_code)
-    
+
     if (response.status_code == 404):
         return 0
     res = response.json().get('data')
