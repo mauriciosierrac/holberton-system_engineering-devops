38b273fbc2751b12903a265fa3432a85ed186b58
mauriciosierrac <2376@holbertonschool.com>
2021-05-27 10:03:08 -0500

    file: update number of limit request

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,10 @@
 # fix number of request
-exec { 'More_Request':
-        command => 'sed -i s/15/5000/ /etc/default/nginx',
-        path => '/usr/local/bin/:/bin/',
-        provider => shell,
- }
- 
-exec { 'restart-nginx-service':
-  command => 'nginx restart',
-  path    => '/etc/init.d/',
+exec { 'sed -i s/15/5000/g /etc/default/nginx':
+  path     => '/usr/bin:/usr/sbin:/bin',
+  provider => shell,
+}
+
+exec { 'sudo service nginx restart':
+  path     => '/usr/bin:/usr/sbin:/bin',
   provider => shell,
 }
